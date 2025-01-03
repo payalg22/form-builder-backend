57b7eb2a3a77cbaa6a969f6e44d186f91b0a7c0d
Payal Gaikwad <gaikwadpayalr@gmail.com>
2025-01-03 19:46:56 +0530

    Final changes

--- middleware/validate.js
+++ middleware/validate.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 
 const isValidId = (req, res, next) => {
   const { id } = req.params;
-  //const isValid = mongoose.isValidObjectId(id);
   const isValid = mongoose.Types.ObjectId.isValid(id);
   if (!isValid) {
     return res.status(400).json({
--- utils/isAuth.js
+++ utils/isAuth.js
@@ -3,20 +3,12 @@ function isAuth(userid, workspace) {
     (item) => item.user.toString() === userid.toString()
   );
   const isOwner = workspace.owner.toString() === userid.toString();
-  
-  if(!isOwner && isShared === undefined){
+
+  if (!isOwner && isShared === undefined) {
     return false;
   }
 
   return isOwner ? true : isShared;
 }
 
-function isEditor(userid, workspace) {
-  const isShared = workspace.sharedTo.find(
-    (item) => item.user.toString() === userid.toString()
-  );
-
-  return isShared.isEditor;
-}
-
-module.exports = { isAuth, isEditor };
+module.exports = { isAuth };
